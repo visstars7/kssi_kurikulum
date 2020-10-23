432da394e6acc347e3d084deda05bae81e7cdcb6
novendra27 <67815092+novendra27@users.noreply.github.com>
2020-10-23 09:30:51 +0700

    Membuat sidebar admin perpustakaan

---
+++ application/modules/Perpustakaan/views/v_perpustakaan.blade.php
@@ -0,0 +1,15 @@
+@extends('template.master')
+@section('title','Humas')
+@section('user-role')
+<div class="d-flex justify-content-start mx-5">
+    <h3 style="font-size: 20px;color:#777d9b;margin-top:13%;">Hallo, Admin Humas</h3>
+</div>
+@endsection
+@section('sidebar')
+<li class="kt-menu__item my-1" aria-haspopup="true"><a target="_blank" href="https://keenthemes.com/keen/preview/demo1/builder.html" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-book"></i><span class="kt-menu__link-text">E-book</span></a></li>
+@endsection
+@section('content')
+<div style="margin-left:30px;margin-top:30px">
+    <h2>Ini adalah view dari kesiswaan </h2>
+</div>
+@endsection
