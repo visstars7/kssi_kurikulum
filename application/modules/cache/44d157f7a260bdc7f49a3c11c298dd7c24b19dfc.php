12c546047f60fc6d133e2f5a1a9d6b48d46078ac
visstars7 <havisikkubaru@gmail.com>
2020-11-10 08:31:48 -0500

    v2.22.11 / Kurikulum / Build Sesi Pelajaran

--- application/modules/cache/44d157f7a260bdc7f49a3c11c298dd7c24b19dfc.php
+++ application/modules/cache/44d157f7a260bdc7f49a3c11c298dd7c24b19dfc.php
@@ -1,4 +1,3 @@
-;
 <?php $__env->startSection('content'); ?>
 <div class="container-fluid">
     <div class="row mt-5">
--- application/modules/cache/9bb064913a40c40086cdb67e087ec564cb53c632.php
+++ application/modules/cache/9bb064913a40c40086cdb67e087ec564cb53c632.php
@@ -4,14 +4,13 @@
 </div>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('sidebar'); ?>
-<li class="kt-menu__item my-1 <?= $activeSide == 'dashboard' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="https://keenthemes.com/keen/preview/demo1/builder.html" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-tachometer-alt"></i><span class="kt-menu__link-text">Dashboard</span></a> </li>
+<li class="kt-menu__item my-1 <?= $activeSide == 'dashboard' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum') ?>" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-tachometer-alt"></i><span class="kt-menu__link-text">Dashboard</span></a> </li>
 <li class="kt-menu__item  kt-menu__item--submenu my-1 <?= $activeSide == 'absensi_kbm' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><i class="kt-menu__link-icon fas fa-clipboard-list" style="margin-left:-3px;"></i><span class="kt-menu__link-text" style="margin-left:3px;">Absensi KBM</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item kt-menu__item--parent <?= $activeSide == 'absensi_kbm' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Absensi KBM</span></span></li>
-            <li class="kt-menu__item <?= $activeSide == 'absensi_siswa' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Absensi Siswa</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'absensi_guru' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/navy-header.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Absensi Guru</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'jurnal' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Jurnal</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'absensi_siswa' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/absensi-siswa') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Absensi Siswa</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'absensi_guru' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/absensi-guru') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Absensi Guru</span></a></li>
         </ul>
     </div>
 </li>
@@ -28,8 +27,8 @@
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item kt-menu__item--parent <?= $activeSide == 'pjj' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">PJJ</span></span></li>
-            <li class="kt-menu__item <?= $activeSide == 'materi' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Materi</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'penyerahan_materi' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/navy-header.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Penyerahan Materi</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'materi' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/materi') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Materi</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'penyerahan_materi' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/penyerahan-materi') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Penyerahan Materi</span></a></li>
         </ul>
     </div>
 </li>
@@ -37,12 +36,13 @@
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item kt-menu__item--parent <?= $activeSide == 'jadwal' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Jadwal</span></span></li>
-            <li class="kt-menu__item <?= $activeSide == 'jadwal_menu' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Jadwal</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'ruang' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Ruang</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'hari_pelajaran' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link"><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Hari Pelajaran</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'guru_mapel' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Guru Mapel</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'sesi_pelajaran' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Sesi Pelajaran</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'mapel' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Mapel</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'jadwal_menu' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/jadwal') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Jadwal</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'jurnal' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/jurnal') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Jurnal</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'ruang' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/ruang') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Ruang</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'hari_pelajaran' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/hari-pelajaran') ?>" class="kt-menu__link"><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Hari Pelajaran</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'guru_mapel' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/guru-mapel') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Guru Mapel</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'sesi_pelajaran' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/sesi-pelajaran') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Sesi Pelajaran</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'mapel' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/mapel') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Mapel</span></a></li>
         </ul>
     </div>
 </li>
@@ -50,11 +50,11 @@
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item kt-menu__item--parent <?= $activeSide == 'mengelola_nilai' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Mengelola Nilai</span></span></li>
-            <li class="kt-menu__item <?= $activeSide == 'daftar_nilai' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/brand-aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Daftar Nilai</span></a></li>
-            <li class="kt-menu__item <?= $activeSide == 'pengelola_nilai' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="dashboards/navy-header.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pengelola Nilai</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'daftar_nilai' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/daftar-nilai') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Daftar Nilai</span></a></li>
+            <li class="kt-menu__item <?= $activeSide == 'pengelola_nilai' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/pengelola-nilai') ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pengelola Nilai</span></a></li>
         </ul>
     </div>
 </li>
-<li class="kt-menu__item my-1 <?= $activeSide == 'e_rapot' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="https://keenthemes.com/keen/preview/demo1/builder.html" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-award"></i><span class="kt-menu__link-text">E-rapot</span></a></li>
+<li class="kt-menu__item my-1 <?= $activeSide == 'e_rapot' ? 'kt-menu__item--open' : false ?>" aria-haspopup="true"><a href="<?= base_url('Kurikulum/e-rapot') ?>" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-award"></i><span class="kt-menu__link-text">E-rapot</span></a></li>
 
 <?php $__env->stopSection(); ?><?php /**PATH /opt/lampp/htdocs/kssi_kurikulum/application/modules/Kurikulum/views/sidebar.blade.php ENDPATH**/ ?>
