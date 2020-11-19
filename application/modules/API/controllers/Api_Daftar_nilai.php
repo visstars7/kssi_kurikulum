683d03c0f5f6b2b4ad58fd805ad3d8b8c2df2375
visstars7 <havisikkubaru@gmail.com>
2020-11-19 12:45:58 -0500

    v2.32.15 / Kurikulum / Feature API Daftar Nilai

--- application/modules/API/controllers/Api_Daftar_nilai.php
+++ application/modules/API/controllers/Api_Daftar_nilai.php
@@ -26,6 +26,7 @@ class Daftar_nilai extends MX_Controller
 
     private function _toExcel($data)
     {
+        $arr = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
 
@@ -46,12 +47,11 @@ class Daftar_nilai extends MX_Controller
         $sheet->setCellValue('A6', 'No');
         $sheet->setCellValue('B6', 'NIP');
         $sheet->setCellValue('C6', 'Nama Siswa');
-        $sheet->setCellValue('D6', 'UH1');
-        $sheet->setCellValue('E6', 'UH2');
-        $sheet->setCellValue('F6', 'UH3');
-        $sheet->setCellValue('G6', 'UH4');
-        $sheet->setCellValue('H6', 'UTS');
-        $sheet->setCellValue('I6', 'UAS');
+
+        for ($i = 1; $i <= count($data['jenis_uji']); $i++) {
+            $j = $i - 1;
+            $sheet->setCellValue("$arr[$j]6", $data['jenis_uji'][$j]);
+        }
 
         foreach ($data['siswa'] as $field) {
             $sheet->setCellValue("A$noSheet", $no);
@@ -77,6 +77,7 @@ class Daftar_nilai extends MX_Controller
         $post = $this->input->post();;
         $data = [
             'siswa' => $this->M_daftar_nilai->get_where('master', 'siswa', ['kelas_id' => $post['id_kelas']]),
+            'jenis_uji' => $post['jenis_uji'],
             'kelas' => $this->M_daftar_nilai->get_where('master', 'kelas', ['id' => $post['id_kelas']]),
             'guru_mapel' => $this->M_daftar_nilai->get_where('db', 'v_guru_mapel', ['id_guru_mapel' => $post['id_guru_mapel']]),
             'semester' => $post['semester']
@@ -99,34 +100,28 @@ class Daftar_nilai extends MX_Controller
         $post = $_FILES['nilai_siswa'];
 
         $result = $this->_readExcel($post);
-
         $row = [];
         for ($i = 6; $i < count($result); $i++) {
-            $data = [
-                'nisn'  => $result[$i][1],
-                'UH1'   => $result[$i][3],
-                'UH2'   => $result[$i][4],
-                'UH3'   => $result[$i][5],
-                'UH4'   => $result[$i][6],
-                'UTS'   => $result[$i][7],
-                'UAS'   => $result[$i][8],
-                'id_guru_mapel' => $result[1][2],
-                'id_kelas' => $result[0][2],
-                'semester' => $result[3][1],
-                'create_at' => date('Y-m-d H:i:s')
-            ];
-            $row[] = $data;
+            for ($j = 3; $j < count($result[5]); $j++) {
+                $data = [
+                    'nisn'  => $result[$i][1],
+                    'id_guru_mapel' => $result[1][2],
+                    'id_kelas' => $result[0][2],
+                    'jenis_uji' => $result[5][$j],
+                    'nilai' => $result[$i][$j],
+                    'semester' => $result[3][1],
+                    'create_at' => date('Y-m-d H:i:s')
+                ];
+                $row[] = $data;
+            }
         }
         if ($this->M_daftar_nilai->insert_batch('tb_penilaian', $row)) {
-            $data = [
+            $json = [
                 'result' => 'success'
             ];
-            echo json_encode($data);
+            echo json_encode($json);
         } else {
-            $data = [
-                'result' => 'failed'
-            ];
-            echo json_encode($data);
+            show_404();
         }
     }
 };;
