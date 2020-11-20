7765deca38887f7106a4040553810773f6b4c700
visstars7 <havisikkubaru@gmail.com>
2020-11-20 16:40:04 -0500

    v2.33.16 / Kurikulum / feature rapot

--- application/modules/Kurikulum/models/M_e_rapot.php
+++ application/modules/Kurikulum/models/M_e_rapot.php
@@ -2,8 +2,46 @@
 
 class M_e_rapot extends CI_model
 {
+    private $master;
     public function __construct()
     {
         parent::__construct();
+        $this->master = $this->load->database('db_master', TRUE);
+    }
+
+    public function get($db, $tb)
+    {
+        return $this->$db->get($tb)->result_array();
+    }
+
+    public function get_where($db, $tb, $where, $type)
+    {
+        if ($type == 'rata-rata') {
+
+            $kelas = $where['id_kelas'];
+            $semester = $where['semester'];
+
+            return $this->$db->query("SELECT * FROM $tb WHERE `id_kelas` = $kelas
+            AND `semester` = $semester AND `jenis_uji` != 'PAS' AND `jenis_uji` != 'PTS'")->result_array();
+        } elseif ($type == 'ujian') {
+
+            $kelas = $where['id_kelas'];
+            $semester = $where['semester'];
+
+            return $this->$db->query("SELECT * FROM $tb WHERE `id_kelas` = $kelas
+            AND `semester` = $semester AND `jenis_uji` = 'PAS' OR `jenis_uji` = 'PTS'")->result_array();
+        } else {
+            return $this->$db->get_where($tb, $where)->result_array();
+        }
+    }
+
+    public function get_distinct($db, $tb, $id_kelas, $type)
+    {
+        if ($type == 'jenis_uji') {
+
+            return $this->$db->query("SELECT COUNT(DISTINCT `$type`) FROM $tb WHERE `id_kelas` = $id_kelas")->result_array();
+        } else {
+            return $this->$db->query("SELECT DISTINCT `$type` FROM $tb WHERE `id_kelas` = $id_kelas")->result_array();
+        }
     }
 };
--- application/modules/cache/d3a873a709e4140a7fb252ddc5ee25cdc4f5e0cb.php
+++ application/modules/cache/d3a873a709e4140a7fb252ddc5ee25cdc4f5e0cb.php
@@ -1,6 +1,72 @@
 <?php $__env->startSection('content'); ?>
-<div style="margin-left:30px;margin-top:30px">
-    <h2>Ini adalah view dari e-rapot</h2>
+<div class="container-fluid">
+    <form id="form" action="<?= base_url('Kurikulum/E_rapot/store') ?>" method="post">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <button onclick="cetak()" class="btn btn-primary float-right" data-toggle="modal" data-target="#create"><i class="fa fa-file"></i> <strong>Cetak</strong></button>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="siswa">Kelas</label>
+                            <select class="form-control" name="kelas" id="kelas"></select>
+                        </div>
+                        <input type="hidden" name="db" value="db"></input>
+                        <input type="hidden" name="tb" value="v_penilaian"></input>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="siswa">Semester</label>
+                            <select class="form-control" name="semester" id="smt">
+                                <option value="1">Ganjil</option>
+                                <option value="2">Genap</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="siswa">Tahun ajaran</label>
+                            <select class="form-control" name="tahun_ajaran" id="tahun"></select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
+<script>
+    $(document).ready(() => {
+        // kelas
+        $.ajax({
+            data: `db=master&tb=kelas`,
+            url: `<?= base_url('Kurikulum/E_rapot/get') ?>`,
+            type: `post`,
+            dataType: `json`,
+            success: (res) => {
+                var optKelas = [];
+                $.each(res, (idx, val) => {
+                    optKelas += `<option value="${val.id}">${val.nama}</option>`;
+                });
+                $("#kelas").html(optKelas);
+            }
+        });
+
+        // tahun
+        var date = new Date();
+        var year = date.getFullYear();
+        var optTahun = [];
+        for (var i = year; i > 2001; i--) {
+            optTahun += `<option value="${i}">${i}</option>`;
+        }
+        $("#tahun").html(optTahun);
+    })
+
+    $("#kelas").select2();
+    $("#smt").select2();
+    $("#tahun").select2();
+</script>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('Kurikulum.views.kurikulum', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/kssi_kurikulum/application/modules/Kurikulum/views/e_rapot.blade.php ENDPATH**/ ?>
