47b0ec40a2f557f82b4ecaefdf23514a7e00842f
visstars7 <havisikkubaru@gmail.com>
2020-11-24 19:05:17 -0500

    v2.44.19 / Kurikulum / Feature read jurnal in server

--- application/modules/Kurikulum/controllers/Jurnal.php
+++ application/modules/Kurikulum/controllers/Jurnal.php
@@ -15,5 +15,40 @@ class Jurnal extends MX_Controller
             'activeSide' => 'jurnal'
         ];
         return view('Kurikulum.views.jurnal', $data);
-	}
+    }
+
+
+    // Datatables
+    public function jurnal_datatables()
+    {
+        if ($this->input->is_ajax_request() == true) {
+
+            $list = $this->M_jurnal->get_datatables('v_jurnal');
+            $data = array();
+            $no = $_POST['start'];
+
+            foreach ($list as $field) {
+
+                $no++;
+                $row[] = $no;
+                $row[] = $field->nama_guru;
+                $row[] = $field->nama_mapel;
+                $row[] = $field->kegiatan;
+                $row[] = $field->nama_kelas;
+                $row[] = $field->create_at;
+                $data[] = $row;
+            }
+
+            $output = array(
+                "draw" => $_POST['draw'],
+                "recordsTotal" => $this->M_jurnal->count_all(),
+                "recordsFiltered" => $this->M_jurnal->count_filtered(),
+                "data" => $data,
+            );
+            //output dalam format JSON
+            echo json_encode($output);
+        } else {
+            show_404();
+        }
+    }
 };
--- application/modules/cache/80ee3ba903afa020a397bb4decd7c171b3dd4772.php
+++ application/modules/cache/80ee3ba903afa020a397bb4decd7c171b3dd4772.php
@@ -1,6 +1,47 @@
 <?php $__env->startSection('content'); ?>
-<div style="margin-left:30px;margin-top:30px">
-    <h2>Ini adalah view dari jurnal</h2>
+<div class="container-fluid">
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <table id="table" class="table table-hover table-striped table-borderless">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama guru</th>
+                        <th>Nama mapel</th>
+                        <th>Nama kelas</th>
+                        <th>Kegiatan</th>
+                        <th>Tgl Dibuat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+
+
+<script>
+    $('#table').DataTable({
+        responsive: true,
+        "destroy": true,
+        "processing": true,
+        "serverSide": true,
+        "order": [],
+
+        "ajax": {
+            "url": "<?= base_url('Kurikulum/Jurnal/jurnal_datatables') ?>",
+            "type": "POST"
+        },
+
+
+        "columnDefs": [{
+            "targets": [0],
+            "orderable": false,
+            "width": 5
+        }],
+
+    });
+</script>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('Kurikulum.views.kurikulum', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/kssi_kurikulum/application/modules/Kurikulum/views/jurnal.blade.php ENDPATH**/ ?>
